0159ff170db4f4cd19ac1d2025dafa2d30c39777
Mussie Nadew <muchichi@gmail.com>
2017-04-18 21:49:32 -0400

    Readme update and minor comment on functions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # py-nba
-A python exploratory examples on nba teams and players
+A python exploratory examples on nba teams and players.
+This python script focuses on the Toronto Raptors team for the year 2016/2017.
+- Comming up:
+-   Team performance against other NBA teams
+-   Players performance as compared to previous games
+-   Performance amongst players
+-   High and Low score factors(home field and away)
--- nba_stat.py
+++ nba_stat.py
@@ -7,14 +7,15 @@ def nba_main():
     raptors = team.TeamPassTracking(raptors_id)
     rp = raptors.passes_made().head()
     print(rp)
-    #Raptors last game passes
+    
+    #Raptors last game 2 passes in the playo-ff
 def nba_passes():
-    raptors_last_game = team.TeamPassTracking(raptors_id,last_n_games=1)
+    raptors_last_game = team.TeamPassTracking(raptors_id,last_n_games=2)
     raptors_last_game.passes_made().head()
     
-    #Last game team shot tracking
+    #Last game team shot tracking for the playoff
 def nba_shots():
-    raptors_shots = team.TeamShotTracking(raptors_id,last_n_games=1)
+    raptors_shots = team.TeamShotTracking(raptors_id,last_n_games=7)
     raptors_shots.closest_defender_shooting()
 
 if __name__ == '__main__': nba_main()
